f0d43d0a6291b84261daa4ffe48b8616a3a498d1
Anne Odom <afodom207@gmail.com>
2025-11-30 11:13:42 -0600

    Add: Watchmode API fetch code

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,31 @@
+//----WATCHMODE API IMPLEMENTATION----
+const watchModeAPIKey = "FtuHOv5sr92FjZGx2SHVTAbsPw8etFsPcJ8gYsin";
+//we are going to search the name field for matches for the searchValue
+const searchField = "name";
+//this will not stay hardcoded, will be assigned based on user request
+let searchValue = "one piece";
+//change any spaces in the search value into %20 for valid search query
+searchValue = searchValue.replace(" ", "%20");
+console.log(`searchValue: ${searchValue}`);
+//build the URL
+let wmRequestURL = `https://api.watchmode.com/v1/search?search_field=${searchField}&search_value=${searchValue}&apiKey=${watchModeAPIKey}`;
+console.log("URL Request:", wmRequestURL);
+
+//fetch instances of searchValue from watchmode
+fetch(wmRequestURL)
+  .then(function (response) {
+    console.log("Raw Response Object:", response);
+    console.log(`HTTP Status: ${response.status}`);
+    return response.json();
+  })
+  .then(function (data) {
+    console.log("Parsed JSON Data: ", data);
+  })
+  .catch(function (error) {
+    console.error(`Network or fetch error: ${error}`);
+  });
+
+//----EVENT LISTENERS AND INTERFACE BEHAVIOR-----
 // We use 'load' instead of 'DOMContentLoaded' to ensure CSS and Images
 // are fully loaded so we can calculate the correct heights.
 window.addEventListener("load", () => {
