be4400a3f4f9f3bca6300dfa6ef7823ea4c2909a
Anne Odom <afodom207@gmail.com>
2025-12-06 11:47:16 -0600

    Modify: complete getCleanString to adapt for all watchmode object properties that have null or undefined values

--- assets/js/temp_anneworking.js
+++ assets/js/temp_anneworking.js
@@ -89,6 +89,20 @@ let watchmode = [
     network_names: null,
     trailer: null,
   },
+  {
+    id: "2368682",
+    title: "null",
+    plot_overview: "null",
+    type: "short_film",
+    runtime_minutes: "null",
+    release_date: "2002-03-02",
+    genre_names: null,
+    user_rating: "null",
+    critic_score: null,
+    poster: "null",
+    network_names: null,
+    trailer: null,
+  },
 ];
 //console.log(watchmode);
 
@@ -104,24 +118,93 @@ function convertToListItem(liContent) {
   return liEl;
 }
 
+//checks for empty or null content for an item from a watchmode object
+//if it is empty or null, returns a replacement content string
+//Parameters:
+//key - the name of the property in the object to test
+//content - the content that is associated with that key
 function getCleanString(key, content) {
   // console.log(
-  //   `Checking for null for with key: ${key} and content ${content}`
+  //   `Checking for null for with key: ${key} and content [${content}]`
   // );
+  let emptyContent = false;
+  if (content === null || content == "null" || content == "undefined") {
+    // console.log(`content for ${key} is empty`);
+    emptyContent = true;
+  } else {
+    // console.log(`Content for key: ${key} is [${content}]`);
+  }
   switch (key) {
+    case "title":
+      if (emptyContent) {
+        content = "Untitled";
+      }
+      break;
+    case "plot_overview":
+      if (emptyContent) {
+        content = "Oh no! This one doesn't have a summary!";
+      }
+      break;
+    case "runtime_minutes":
+      if (emptyContent) {
+        content = "Unkown";
+      } else {
+        content = `${content}m`;
+      }
+      break;
+    case "genre_names":
+      if (emptyContent) {
+        content = "";
+      } else {
+        content.join(", ");
+      }
+
+      break;
+    case "user_rating":
+      if (emptyContent) {
+        // console.log(`We have determined ${content} is null or undefined`);
+        content = "No user ratings";
+      } else {
+        // console.log(`We have determined content is fine`);
+        content = `${content}/10`;
+      }
+      break;
     case "critic_score":
-      if (content == "null" || content == "undefined") {
-        //console.log(`We have determined ${content} is null or undefined`);
+      // console.log(`checking for value of empty content: ${emptyContent}`);
+      if (emptyContent) {
+        // console.log(`We have determined ${content} is null or undefined`);
         content = "Not reviewed by critics";
       } else {
-        //console.log(`We have determined content is fine`);
+        // console.log(`We have determined content is fine`);
         content = `${content}/100`;
       }
+      break;
+    case "poster":
+      if (emptyContent) {
+        content = "./assets/images/no_image_available.png";
+      }
+      break;
+    case "network_names":
+      if (emptyContent) {
+        // console.log(`We have determined ${key} is null or undefined`);
+        content = "unavailable for streaming :(";
+      }
+      break;
+    case "trailer":
+      if (emptyContent) {
+        content = "";
+      }
+      break;
+    default:
       break;
   }
 
   return content;
 }
+
+//creates a single card element from a watchmode object and returns it
+//Parameter:
+//watchmode - the object to turn into a card
 function createCard(watchmode) {
   //using the id from the returned data to make unique id for card
   const cardId = `card_${watchmode.id}`;
@@ -136,8 +219,9 @@ function createCard(watchmode) {
   cardDiv.appendChild(cardFigure);
 
   //create the image element with the link to the poster from the watchmode object, append to figure
+  const posterUrl = getCleanString("poster", watchmode.poster);
   const cardPoster = document.createElement("img");
-  cardPoster.setAttribute("src", watchmode.poster);
+  cardPoster.setAttribute("src", posterUrl);
   cardPoster.setAttribute("alt", watchmode.type);
   cardPoster.setAttribute("class", "poster w-24");
   cardFigure.appendChild(cardPoster);
@@ -149,12 +233,15 @@ function createCard(watchmode) {
   //create the card title and set it to the title from the watchmode object. append to card body
   const cardTitle = document.createElement("h2");
   cardTitle.setAttribute("class", "card-title");
-  cardTitle.innerHTML = watchmode.title;
+  cardTitle.innerHTML = getCleanString("title", watchmode.title);
   cardBody.appendChild(cardTitle);
 
   //create cardReleaseDate element, set to release date from watchmode object, append to card body
   let cardReleaseDate = document.createElement("p");
-  cardReleaseDate.innerHTML = watchmode.release_date;
+  cardReleaseDate.innerHTML = getCleanString(
+    "release_date",
+    watchmode.release_date
+  );
   cardBody.appendChild(cardReleaseDate);
 
   //create list to hold other key information like runtime, genres, etc
@@ -162,54 +249,60 @@ function createCard(watchmode) {
   cardList.setAttribute("style", "list-style-type: none");
 
   //create list items to go in the list
-  const genresLi = convertToListItem(watchmode.genre_names.join(", "));
+  const genres = getCleanString("genre_names", watchmode.genre_names);
+  const genresLi = convertToListItem(genres);
   genresLi.style.fontStyle = "italic";
   cardList.appendChild(genresLi);
-  //THIS IS THE RUN TIME STUFF, IT DOES NOT APPLY TO TV SERIES SO
-  //CONDITIONAL STUFF WILL NEED TO HAPPEN HERE
-  const runningTimeLi = convertToListItem(
-    `Running Time: ${watchmode.runtime_minutes}m`
+
+  //create running time list element, set to run time from watchmode object, append to list
+  const runningTime = getCleanString(
+    "runtime_minutes",
+    watchmode.runtime_minutes
   );
+  const runningTimeLi = convertToListItem(`Running Time: ${runningTime}`);
   cardList.appendChild(runningTimeLi);
-  const userRatingLi = convertToListItem(
-    `User Rating: ${watchmode.user_rating}/10`
-  );
+
+  //create user rating list item, set to user rating from watchmode object, append to list
+  const userRating = getCleanString("user_rating", watchmode.user_rating);
+  const userRatingLi = convertToListItem(`User Rating: ${userRating}`);
   cardList.appendChild(userRatingLi);
-  let criticScore = getCleanString(
-    `${watchmode.title}`,
+
+  //create critic score list item, set to critic score from watchmode object, append to list
+  const criticScore = getCleanString(
     "critic_score",
     `${watchmode.critic_score}`
   );
   const criticRatingLi = convertToListItem(`Critic Rating: ${criticScore}`);
   cardList.appendChild(criticRatingLi);
-  let networkNames = "";
-  if (watchmode.network_names === null) {
-    networkNames = "No platforms";
-  } else {
-    networkNames = watchmode.network_names.join(", ");
-  }
+
+  //create network names list item, set to network names from watchmode object, append to list
+  const networkNames = getCleanString("network_names", watchmode.network_names);
   const streamingLi = convertToListItem(`Where to watch: ${networkNames}`);
   cardList.appendChild(streamingLi);
   cardBody.appendChild(cardList);
 
-  //create anchor tag for trailer element, set to trailer url from watchmode object, append to cardBody
-  const cardTrailerUrl = document.createElement("a");
-  cardTrailerUrl.setAttribute("href", watchmode.trailer);
-  cardTrailerUrl.setAttribute(
-    "class",
-    "text-primary hover:underline font-semibold"
-  );
-  cardTrailerUrl.innerHTML = "Watch Trailer";
-  cardBody.appendChild(cardTrailerUrl);
+  //check for empty trailer item on watchmode object. If not empty, create anchor tag for trailer element, set to trailer url from watchmode object, append to cardBody. Else do not include any trailer informaiton
+  const cardTrailerUrl = getCleanString("trailer", watchmode.trailer);
+  if (cardTrailerUrl != "") {
+    const cardTrailerAnchor = document.createElement("a");
+    cardTrailerAnchor.setAttribute("href", cardTrailerUrl);
+    cardTrailerAnchor.setAttribute(
+      "class",
+      "text-primary hover:underline font-semibold"
+    );
+    cardTrailerAnchor.innerHTML = "Watch Trailer";
+    cardBody.appendChild(cardTrailerAnchor);
+  }
 
-  //create cardDescription element to hold plot overview, set to overview from watchmode object
-  let cardDescriptionDiv = document.createElement("div");
+  //create cardDescription button with hover, set tooltip to overview from watchmode object
+  const plotOverview = getCleanString("plot_overview", watchmode.plot_overview);
+  const cardDescriptionDiv = document.createElement("div");
   cardDescriptionDiv.setAttribute("class", "tooltip tooltip-bottom");
-  cardDescriptionDiv.setAttribute("data-tip", watchmode.plot_overview);
-  let descriptionHoverEl = document.createElement("button");
-  descriptionHoverEl.setAttribute("class", "btn btn-primary");
-  descriptionHoverEl.innerHTML = "Read Summary";
-  cardDescriptionDiv.appendChild(descriptionHoverEl);
+  cardDescriptionDiv.setAttribute("data-tip", plotOverview);
+  const descriptionHoverButton = document.createElement("button");
+  descriptionHoverButton.setAttribute("class", "btn btn-primary");
+  descriptionHoverButton.innerHTML = "Read Summary";
+  cardDescriptionDiv.appendChild(descriptionHoverButton);
   cardBody.appendChild(cardDescriptionDiv);
 
   //Append the complete card body to the div
@@ -217,6 +310,10 @@ function createCard(watchmode) {
   return cardDiv;
 }
 
+//sets content of tab label
+//Parameters:
+//id - element id for tab
+//label - content to put on tab
 function setTabLabel(id, label) {
   const tab = document.getElementById(id);
   tab.setAttribute("aria-label", label);
@@ -243,7 +340,7 @@ for (i = 0; i < watchmode.length; i++) {
   const card = createCard(watchmode[i]);
   // console.log(`${watchmode[i].title} is ${watchmode[i].type}`);
 
-  //add the card to the correct tab (this will be a case statement checking watchmode object type and appending based on that)
+  //add the card to the correct tab depending on type
   switch (watchmode[i].type) {
     case "movie":
       movieGridEl.appendChild(card);
